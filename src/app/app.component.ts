3f1d7988ff1a050ce515029efed29652d932c9e4
FerozAhmadLonpo <ajjadhussain786@gmail.com>
2019-12-31 01:40:33 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
+import { forbiddenNameValidator } from "./shared/user-name.validators";
+import { PasswordValidator } from "./shared/password.validators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  constructor(private fb: FormBuilder) {}
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl("Feroz"),
+  //   password: new FormControl(""),
+  //   confirmPassword: new FormControl(""),
+  //   address: new FormGroup({
+  //     city: new FormControl(""),
+  //     state: new FormControl(""),
+  //     postalCode: new FormControl("")
+  //   })
+  // });
+
+  registrationForm = this.fb.group(
+    {
+      userName: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          forbiddenNameValidator(/password/)
+        ]
+      ],
+      password: [""],
+      confirmPassword: [""],
+      address: this.fb.group({
+        city: [""],
+        state: [""],
+        postalCode: [""]
+      })
+    },
+    { validators: PasswordValidator }
+  );
+
+  loadApiData() {
+    // this.registrationForm.setValue({
+    //   userName: "Bruce",
+    //   password: "test",
+    //   confirmPassword: "test",
+    //   address: {
+    //     city: "City",
+    //     state: "State",
+    //     postalCode: "123456"
+    //   }
+    // });
+
+    this.registrationForm.patchValue({
+      userName: "Bruce",
+      password: "test",
+      confirmPassword: "test"
+    });
+  }
 }
